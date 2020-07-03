bd09591f9836fa2d2a009b945ffd014abde52ab1
Yurie Shiotani <yurieshiotani@gmail.com>
2020-07-04 01:17:41 +0900

    seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts "Cleaning up data.."
+Ingredient.destroy_all
+
+puts "Creating data.."
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+drinks_serialized = open(url).read
+drinks = JSON.parse(drinks_serialized)
+
+drinks["drinks"].each_with_index do |drink, index|
+  Ingredient.create(name: drink["strIngredient1"])
+  puts "drink #{index + 1} created"
+end
+
+puts "Done"
